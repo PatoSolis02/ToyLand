739c5782e799f888ac7f036f37e6670ef33837f1
pgs5983 <pgs5983@rit.edu>
2022-09-18 13:24:45 -0400

    Got TestPlayDough to pass all tests. Finished inlab.

--- src/toys/PlayDough.java
+++ src/toys/PlayDough.java
@@ -1,14 +1,33 @@
 package toys;
 
 public class PlayDough extends Toy{
+
+    private final static double WEAR_MULTIPLIER = 0.05;
+    private static int nextId = 100;
+    private Color color;
+
     protected PlayDough(String name, Color color){
-        super(0, name);
+
+        super(nextId, name);
+        this.color = color;
+        nextId++;
+
     }
 
-    public String getColor(){return null;}
+    public Color getColor(){return this.color;}
 
-    protected void specialPlay(int time){}
+    protected void specialPlay(int time){
+        System.out.println("PLAYING(" + time + "): " + toString());
+        System.out.println("\tArts and crafting with " +
+                            this.color +
+                            " " + this.getName());
+        increaseWear(time * WEAR_MULTIPLIER);
+    }
 
     @Override
-    public String toString() {return null;}
+    public String toString() {
+        return super.toString() +
+                ", PlayDough{C:" +
+                this.color + "}";
+    }
 }
--- src/toys/Toy.java
+++ src/toys/Toy.java
@@ -1,32 +1,61 @@
 package toys;
 
 public abstract class Toy implements IToy {
-    protected Toy(int product, String name){};
+    private final static int INITIAL_HAPPINESS = 0;
+    private final static int MAX_HAPPINESS = 100;
+    private final static double INITIAL_WEAR = 0.0;
+    private double wear = 0;
+    private int happiness = 0;
+    private int code;
+    private String name;
+
+    protected Toy(int product, String name){
+        this.code = product;
+        this.name = name;
+    };
 
     @Override
-    public int getProductCode() {return 0;}
+    public int getProductCode() {return this.code;}
 
     @Override
-    public String getName() {return null;}
+    public String getName() {return this.name;}
 
     @Override
-    public int getHappiness() {return 0;}
+    public int getHappiness() {return this.happiness;}
 
     @Override
-    public boolean isRetired() {return false;}
+    public boolean isRetired() {
+        if(getHappiness() > MAX_HAPPINESS) {
+            return true;
+        }
+        return false;
+    }
 
     @Override
-    public double getWear() {return 0;}
+    public double getWear() {return this.wear;}
 
     @Override
-    public void increaseWear(double amount) {}
+    public void increaseWear(double amount) {
+        this.wear += amount;
+    }
 
     @Override
-    public void play(int minutes) {}
+    public void play(int minutes) {
+        specialPlay(minutes);
+        this.happiness += minutes;
+        if(isRetired()){
+            System.out.println("RETIRED: " + toString());
+        }
+    }
 
     protected abstract void specialPlay(int minutes);
 
     @Override
-    public String toString() {return null;}
+    public String toString() {
+        return "Toy{PC:" + this.code + ", N:"
+                + this.name + ", H:" + this.happiness
+                + ", R:" + this.isRetired()
+                + ", W:" + this.wear + "}";
+    }
 
 }
