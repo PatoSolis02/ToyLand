8891598ffe0ad303f874ba9c8ea0d6dc98545243
pgs5983 <solis.patricio02@gmail.com>
2022-09-19 16:58:55 -0400

    Add docstrings to Doll and PlayDough.

--- src/toys/Doll.java
+++ src/toys/Doll.java
@@ -1,10 +1,26 @@
 package toys;
 
 public class Doll extends Toy{
+    /** color of the Doll's hair */
     private Color hairColor;
+    /** age of the doll */
     private int age;
+    /** the noise produced by the doll */
     private String speak;
+    /** start of unique id for each Doll created */
     private static int code = 200;
+
+
+    /**
+     * Doll's constructor. Calls the constructor of its parent class
+     * and initializes the hair color, age, and speak of doll and
+     * increments the unique id after each instance created.
+     *
+     * @param name String the name of the doll
+     * @param hairColor Color the color of the doll's hair (from enum class Color)
+     * @param age int the age of the doll
+     * @param speak String the sounds produced by dolls
+     */
     protected Doll(String name, Color hairColor, int age, String speak){
         super(code, name);
         this.age = age;
@@ -13,12 +29,38 @@ public class Doll extends Toy{
         code++;
     }
 
+    /**
+     * accessor for the hair color of Doll
+     *
+     * @return Color the hair color of specific instance of Doll
+     */
     public Color getHairColor(){return this.hairColor;}
 
+
+    /**
+     * accessor for the age of the doll
+     *
+     * @return int the age of specific instance of Doll
+     */
     public int getAge(){return this.age;}
 
+
+    /**
+     * accessor for the sound produced by Doll
+     *
+     * @return String the sound produced by specific instance of Doll
+     */
     public String getSpeak(){return this.speak;}
 
+
+    /**
+     * The specific actions Doll takes when it is played with.
+     * Prints out a unique play statement that describes what is getting
+     * played with and how they are getting played with. Also increments
+     * the specific instance's wear.
+     *
+     * @param minutes int the amount of time the Doll was played with
+     */
     @Override
     protected void specialPlay(int minutes) {
         System.out.println("\t" + getName() + " brushes their " + getHairColor()
@@ -26,6 +68,12 @@ public class Doll extends Toy{
         increaseWear(age);
     }
 
+    /**
+     * Formatted output of Toy's toString() with specific information that pertains
+     * to Doll.
+     *
+     * @return String that includes all the desired toy and Doll's information
+     */
     @Override
     public String toString(){
         return super.toString() +
--- src/toys/PlayDough.java
+++ src/toys/PlayDough.java
@@ -2,20 +2,44 @@ package toys;
 
 public class PlayDough extends Toy{
 
+    /** factor to calculate amount of wear */
     private final static double WEAR_MULTIPLIER = 0.05;
+    /** start of unique id for each PlayDough created */
     private static int nextId = 100;
+    /** the color of the PlayDough */
     private Color color;
 
+    /**
+     * PlayDough's constructor. Calls the constructor of its parent class
+     * and initializes the color of playdough and increments the unique id
+     * after each instance created.
+     *
+     * @param name String the name of the object
+     * @param color Color the color of the PlayDough (from enum class Color)
+     */
     protected PlayDough(String name, Color color){
-
         super(nextId, name);
         this.color = color;
         nextId++;
-
     }
 
+
+    /**
+     * accessor for the color of PlayDough
+     *
+     * @return Color the color of specific instance of PlayDough
+     */
     public Color getColor(){return this.color;}
 
+
+    /**
+     * The specific actions PlayDough takes when it is played with.
+     * Prints out a unique play statement that describes what is getting
+     * played with and how they are getting played with. Also increments
+     * the specific instance's wear.
+     *
+     * @param time int the amount of time the PlayDough was played with
+     */
     protected void specialPlay(int time){
         System.out.println("\tArts and crafting with " +
                             this.color +
@@ -23,6 +47,13 @@ public class PlayDough extends Toy{
         increaseWear(time * WEAR_MULTIPLIER);
     }
 
+
+    /**
+     * Formatted output of Toy's toString() with specific information that pertains
+     * to PlayDough.
+     *
+     * @return String that includes all the desired toy and playdough's information
+     */
     @Override
     public String toString() {
         return super.toString() +
